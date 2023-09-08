a0432ebfc56af9c97dfd7288840437d31de40e76
Furkan Emin Can <femincanx@gmail.com>
2023-09-08 16:26:52 +0300

    refactor: export assets from index file

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+export { default as ChildRoom } from './child-room.jpg';
+export { default as JapaneseStreet } from './japanese-street.jpg';
+export { default as LiveRoom } from './live-room.jpg';
+export { default as ParentRoom } from './parent-room.jpg';
+export { default as Penthouse } from './penthouse.jpg';
+export { default as SunsetMountain } from './sunset-mountain.jpg';
