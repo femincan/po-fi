d9136e125e9e74332ff544947e5c38798e672a50
Furkan Emin Can <femincanx@gmail.com>
2023-09-08 20:43:44 +0300

    feat: add imageUrl and author properties to backgrounds constant

--- src/constants/backgrounds.ts
+++ src/constants/backgrounds.ts
@@ -7,11 +7,54 @@ import {
   SunsetMountain,
 } from '@src/assets';
 
-export const backgrounds = [
-  { id: 'child-room', src: ChildRoom },
-  { id: 'japanese-street', src: JapaneseStreet },
-  { id: 'live-room', src: LiveRoom },
-  { id: 'parent-room', src: ParentRoom },
-  { id: 'penthouse', src: Penthouse },
-  { id: 'sunset-mountain', src: SunsetMountain },
-] as const;
+type Background = {
+  id: string;
+  src: string;
+  imageUrl: string;
+  author: string;
+};
+
+export const backgrounds: Background[] = [
+  {
+    id: 'child-room',
+    src: ChildRoom,
+    imageUrl:
+      'https://www.freepik.com/free-vector/kids-bedroom-interior-pirate-thematic-night_7588865.htm',
+    author: 'upklyak',
+  },
+  {
+    id: 'japanese-street',
+    src: JapaneseStreet,
+    imageUrl:
+      'https://www.freepik.com/free-photo/bright-pop-landscape-design_21141824.htm',
+    author: 'freepik',
+  },
+  {
+    id: 'living-room',
+    src: LiveRoom,
+    imageUrl:
+      'https://www.freepik.com/free-ai-image/comfortable-armchair-sits-beside-modern-sofa-generated-by-ai_41595400.htm',
+    author: 'vecstock',
+  },
+  {
+    id: 'parent-room',
+    src: ParentRoom,
+    imageUrl:
+      'https://www.freepik.com/free-vector/realist-illustration-room-interior_15066638.htm',
+    author: 'pikisuperstar',
+  },
+  {
+    id: 'penthouse',
+    src: Penthouse,
+    imageUrl:
+      'https://www.freepik.com/free-vector/girl-bedroom-interior-attic-night_7588808.htm',
+    author: 'upklyak',
+  },
+  {
+    id: 'sunset-mountain',
+    src: SunsetMountain,
+    imageUrl:
+      'https://www.freepik.com/free-ai-image/sunset-mountain-range-with-red-sky-sun-setting-it_40964027.htm',
+    author: 'vecstock',
+  },
+];
