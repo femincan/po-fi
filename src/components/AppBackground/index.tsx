c8165b1f40d11caf5dd16a77ee61dbd1b796ffa9
Furkan Emin Can <femincanx@gmail.com>
2023-09-09 10:08:31 +0300

    feat: add AppBackground content and fix layout issues

--- src/components/AppBackground/index.tsx
+++ src/components/AppBackground/index.tsx
@@ -1,16 +1,22 @@
 import { useBackground } from '@src/hooks';
 import { PropsWithChildren } from 'react';
+import { BackgroundCredit } from './BackgroundCredit';
+import { BackgroundSelect } from './BackgroundSelect';
 
 export const AppBackground = ({ children }: PropsWithChildren) => {
   const { currentBg } = useBackground();
 
   return (
     <div className='h-full w-full overflow-y-auto'>
-      {children}
-      <div
-        style={{ backgroundImage: `url(${currentBg.src})` }}
-        className='absolute left-0 top-0 -z-10 h-full w-full bg-cover bg-fixed bg-center'
-      />
+      <div className='relative p-5'>
+        {children}
+        <div
+          style={{ backgroundImage: `url(${currentBg.src})` }}
+          className='absolute left-0 top-0 -z-10 h-full w-full bg-cover bg-fixed bg-center'
+        />
+        <BackgroundSelect />
+        <BackgroundCredit />
+      </div>
     </div>
   );
 };
