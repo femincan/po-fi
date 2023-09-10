c3d84e27a5112638e7ea6048933b9b977dc4a079
Furkan Emin Can <femincanx@gmail.com>
2023-09-10 15:11:29 +0300

    feat: organize assets and add an alarm sound

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,12 +1,41 @@
-export { default as ChildRoom } from './child-room.jpg';
-export { default as JapaneseStreet } from './japanese-street.jpg';
-export { default as LiveRoom } from './living-room.jpg';
-export { default as ParentRoom } from './parent-room.jpg';
-export { default as Penthouse } from './penthouse.jpg';
-export { default as SunsetMountain } from './sunset-mountain.jpg';
+import ChildRoom from './images/child-room.jpg';
+import JapaneseStreet from './images/japanese-street.jpg';
+import LiveRoom from './images/living-room.jpg';
+import ParentRoom from './images/parent-room.jpg';
+import Penthouse from './images/penthouse.jpg';
+import SunsetMountain from './images/sunset-mountain.jpg';
 
-export { ReactComponent as ArrowForward } from './arrow-forward.svg';
-export { ReactComponent as ArrowBack } from './arrow-back.svg';
+import { ReactComponent as ArrowForward } from './icons/arrow-forward.svg';
+import { ReactComponent as ArrowBack } from './icons/arrow-back.svg';
+import { ReactComponent as ArrowUp } from './icons/arrow-up.svg';
+import { ReactComponent as ArrowDown } from './icons/arrow-down.svg';
+import { ReactComponent as Play } from './icons/play.svg';
+import { ReactComponent as Pause } from './icons/pause.svg';
+import { ReactComponent as Reset } from './icons/reset.svg';
 
-export { ReactComponent as ArrowUp } from './arrow-up.svg';
-export { ReactComponent as ArrowDown } from './arrow-down.svg';
+import AlarmSound from './audios/alarm-sound.mp3';
+
+const images = {
+  ChildRoom,
+  JapaneseStreet,
+  LiveRoom,
+  ParentRoom,
+  Penthouse,
+  SunsetMountain,
+};
+
+const icons = {
+  ArrowForward,
+  ArrowBack,
+  ArrowUp,
+  ArrowDown,
+  Play,
+  Pause,
+  Reset,
+};
+
+const audios = {
+  AlarmSound,
+};
+
+export { images, icons, audios };
