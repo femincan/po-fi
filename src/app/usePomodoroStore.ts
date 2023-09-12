a261e1f6be296ec8d15a816caaed5c101a7ec684
Furkan Emin Can <femincanx@gmail.com>
2023-09-12 10:27:06 +0300

    feat: add pomodoro zustand store

---
+++ src/app/usePomodoroStore.ts
@@ -0,0 +1,93 @@
+import { create } from 'zustand';
+
+export type PomodoroState = {
+  sessionTime: number;
+  breakTime: number;
+  remainingTime: number;
+  isPlaying: boolean;
+  isSession: boolean;
+};
+
+export type PomodoroActions = {
+  updateSessionTime: (updatedSessionTime: number) => void;
+  updateBreakTime: (updatedBreakTime: number) => void;
+  decreaseRemainingTimeByOne: () => void;
+  updateRemainingTime: (updatedRemainingTime: number) => void;
+  toggleIsPlaying: () => void;
+  toggleIsSession: () => void;
+  resetState: () => void;
+};
+
+export const defaultState: PomodoroState = {
+  sessionTime: 25,
+  breakTime: 5,
+  remainingTime: 25 * 60,
+  isPlaying: false,
+  isSession: true,
+};
+
+const normalizeTime = (time: number) => {
+  let normalizedTime = time;
+
+  if (time < 1) {
+    normalizedTime = 1;
+  } else if (time > 60) {
+    normalizedTime = 60;
+  }
+
+  return normalizedTime;
+};
+
+export const usePomodoroStore = create<PomodoroState & PomodoroActions>(
+  (set) => ({
+    sessionTime: 25,
+    breakTime: 5,
+    remainingTime: 25 * 60,
+    isPlaying: false,
+    isSession: true,
+    updateSessionTime: (updatedSessionTime) =>
+      set(() => {
+        const normalizedSessionTime = normalizeTime(updatedSessionTime);
+
+        return { sessionTime: normalizedSessionTime };
+      }),
+    updateBreakTime: (updatedBreakTime) =>
+      set(() => {
+        const normalizedBreakTime = normalizeTime(updatedBreakTime);
+
+        return {
+          breakTime: normalizedBreakTime,
+        };
+      }),
+    decreaseRemainingTimeByOne: () =>
+      set(
+        ({
+          sessionTime,
+          breakTime,
+          remainingTime,
+          isSession,
+          toggleIsSession,
+        }) => {
+          let decreasedRemainingTime = remainingTime - 1;
+
+          if (decreasedRemainingTime < 0) {
+            decreasedRemainingTime = (isSession ? breakTime : sessionTime) * 60;
+            toggleIsSession();
+          }
+
+          return { remainingTime: decreasedRemainingTime };
+        },
+      ),
+    updateRemainingTime: (updatedRemainingTime) =>
+      set(() => ({ remainingTime: updatedRemainingTime })),
+    toggleIsPlaying: () =>
+      set(({ isPlaying }) => ({
+        isPlaying: !isPlaying,
+      })),
+    toggleIsSession: () =>
+      set(({ isSession }) => ({
+        isSession: !isSession,
+      })),
+    resetState: () => set(() => defaultState),
+  }),
+);
