6137f9cb2b2caab05999d6d25a5892df0392a65d
Furkan Emin Can <femincanx@gmail.com>
2023-09-12 10:34:40 +0300

    feat: add content of App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,7 @@
-export const App = () => <div>App</div>;
+import { AppBackground, PomodoroTimer } from './components';
+
+export const App = () => (
+  <AppBackground>
+    <PomodoroTimer />
+  </AppBackground>
+);
